7ed1bb94874b40efdc8583d474857a6328d00c06
Wenbo Li <424026452@qq.com>
2022-03-08 13:37:45 +0800

    Improve add_number

--- add_number.py
+++ add_number.py
@@ -9,7 +9,10 @@ for i in range(1, 6):
             for i, line in enumerate(lines):
                 if line.startswith('--'):
                     number += 1
-                    lines[i] = line.replace('-- ', f'-- {number}.')
+                    if line[3].isdigit():
+                        lines[i] = line.replace(line[:line.index('.')], f'-- {number}')
+                    else:
+                        lines[i] = line.replace('-- ', f'-- {number}.')
                     f.seek(0)
                     f.write(''.join(lines))
                     f.truncate()
