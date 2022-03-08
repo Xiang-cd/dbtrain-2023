1cce594e36d075bd82738fb4cca14487cdabcbe9
Wenbo Li <424026452@qq.com>
2022-03-08 14:09:17 +0800

    Set bold in check.py

--- check.py
+++ check.py
@@ -8,9 +8,9 @@ import subprocess
 import sys
 import traceback
 
-RED = '\033[0;31m'
-GREEN = '\033[0;32m'
-YELLOW = '\033[0;33m'
+RED = '\033[1;31m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
 NC = '\033[0m'
 
 class TestCase:
