5c6aa383b2df486873ba10b3d1fcd1d3d8562128
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-04-24 23:41:36 +0800

    Add result

--- lab4/test/20_reorder_with_filter.sql
+++ lab4/test/20_reorder_with_filter.sql
@@ -11,8 +11,5 @@ explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t
 -- t2, t1, t3
 explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t2.temp < 36.0;
 
--- t3, t2, t1
-explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t3.score > 95.0 and t1.score < 200;
-
--- t2, t3, t1
-explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t3.score > 95.0;
+-- t1, t3, t2
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t3.score > 97.0 and t1.score < 200.0;
