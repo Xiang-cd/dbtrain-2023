06ea6f5922c35d2b068db28e5374b2d1780c4ef8
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-04-24 18:04:02 +0800

    Add lab4

---
+++ lab4/test/20_reorder_with_filter.sql
@@ -0,0 +1,16 @@
+use dbtrain_test_lab4;
+
+-- t2, t1, t3
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 10.0;
+
+-- t2, t1, t3
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score > 80.0 and t2.score < 90.0;
+
+-- t2, t1, t3
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t2.temp < 36.0;
+
+-- t3, t2, t1
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t3.score > 95.0 and t1.score < 200;
+
+-- t2, t3, t1
+explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id and t2.score < 30.0 and t3.score > 95.0;
