0ee584b96b006578ffd4700ce1488d9ccdababc1
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-04-25 11:38:19 +0800

    Update lab4 result

--- lab4/test/10_basic_reorder.sql
+++ lab4/test/10_basic_reorder.sql
@@ -2,5 +2,5 @@ use dbtrain_test_lab4;
 
 analyze;
 
--- t1, t2, t3
+-- t1, t2, t3 or t2, t1, t3
 explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id;
--- lab4/test/30_multi_reorder.sql
+++ lab4/test/30_multi_reorder.sql
@@ -12,4 +12,4 @@ explain select t2.id from t1, t2, t3, t4 where t3.score < 80.0 and t4.id = t1.id
 explain select t2.id from t1, t2, t3, t4 where t3.score < 80.0 and t1.id = t2.id and t1.id = t3.id and t1.id = t4.id;
 
 -- t1, t4, t2, t3
-explain select t2.id from t1, t2, t3, t4 where t3.score < 80.0 and t4.id = t1.id and t4.id = t2.id and t4.id = t3.id;
+-- explain select t2.id from t1, t2, t3, t4 where t3.score < 80.0 and t4.id = t1.id and t4.id = t2.id and t4.id = t3.id;
