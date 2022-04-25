0742cc55b95db0a190aa9486715e0a7daca994d3
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-04-26 03:57:04 +0800

    Add test to 10 in lab 4

--- lab4/test/10_basic_reorder.sql
+++ lab4/test/10_basic_reorder.sql
@@ -4,3 +4,6 @@ analyze;
 
 -- t1, t2, t3 or t2, t1, t3
 explain select t2.id from t2, t3, t1 where t2.id = t3.id and t2.id = t1.id;
+
+-- t1, t2, t3 or t2, t1, t3
+explain select t2.id from t2, t3, t1 where t2.id = t1.id and t2.id = t3.id;
