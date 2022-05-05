6ccf6085b11565df6cd3233a03e79e01f2af9802
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-05-05 10:10:37 +0800

    Add new lab 5 test

---
+++ lab5/test/10_insert.sql
@@ -0,0 +1,33 @@
+--    Txn #0 | Txn #1 | Txn #2 |
+--    --------------------------
+--    BEGIN  |        |        |
+--    W(X)   |        |        |
+--    R(X)   |        |        |
+--           | BEGIN  |        |
+--           | R(X)   |        |
+--    COMMIT |        |        |
+--           | R(X)   |        |
+--           | COMMIT |        |
+--           |        | BEGIN  |
+--           |        | R(X)   |
+--           |        | COMMIT |
+
+use dbtrain_test_lab5;
+
+declare t1;
+insert into person values (2, 'b', 36.6);
+select * from person;
+enddecl t1;
+
+declare t2;
+select * from person;
+select * from person;
+enddecl t2;
+
+run t1, t2;
+
+declare t3;
+select * from person;
+enddecl t3;
+
+run t3;
