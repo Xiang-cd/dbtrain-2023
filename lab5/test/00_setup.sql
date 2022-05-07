a3594f53346440588158a49c36343d619e2a70f8
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-05-07 20:08:01 +0800

    Add more tests

--- lab5/test/00_setup.sql
+++ lab5/test/00_setup.sql
@@ -2,8 +2,8 @@ drop database if exists dbtrain_test_lab5;
 create database dbtrain_test_lab5;
 use dbtrain_test_lab5;
 
-create table person(id int, name varchar(20), temperature float);
+create table persons(id int, name varchar(20), temperature float);
 
-insert into person values(1, 'a', 36.1);
-insert into person values(2, 'b', 36.2);
-insert into person values(3, 'c', 36.3);
+insert into persons values(1, 'a', 36.1);
+insert into persons values(2, 'b', 36.2);
+insert into persons values(3, 'c', 36.3);
--- lab5/test/20_update.sql
+++ lab5/test/20_update.sql
@@ -16,8 +16,8 @@ use dbtrain_test_lab5;
 
 declare t1;
   begin;
-  update person set temperature = 37.2 where id = 2;
-  select * from person;
+  update persons set temperature = 37.2 where id = 2;
+  select * from persons;
   signal t1_2;
   wait t2_1;
   commit;
@@ -27,10 +27,10 @@ enddecl t1;
 declare t2;
   wait t1_2;
   begin;
-  select * from person;
+  select * from persons;
   signal t2_1;
   wait t1_c;
-  select * from person;
+  select * from persons;
   commit;
   signal t2_c;
 enddecl t2;
@@ -38,8 +38,54 @@ enddecl t2;
 declare t3;
   wait t2_c;
   begin;
-  select * from person;
+  select * from persons;
   commit;
 enddecl t3;
 
 run t1, t2, t3;
+
+--    Txn #4 | Txn #5 | Txn #6 |
+--    --------------------------
+--    BEGIN  |        |        |
+--           | BEGIN  |        |
+--           | W(X)   |        |
+--    R(X)   |        |        |
+--           | R(X)   |        |
+--           | COMMIT |        |
+--    R(X)   |        |        |
+--    COMMIT |        |        |
+--           |        | BEGIN  |
+--           |        | R(X)   |
+--           |        | COMMIT |
+
+declare t4;
+  begin;
+  signal t4_1;
+  wait t5_1;
+  select * from persons;
+  signal t4_2;
+  wait t5_c;
+  select * from persons;
+  commit;
+  signal t4_c;
+enddecl t4;
+
+declare t5;
+  wait t4_1;
+  begin;
+  update persons set temperature = 37.3 where id = 3;
+  signal t5_1;
+  wait t4_2;
+  select * from persons;
+  commit;
+  signal t5_c;
+enddecl t5;
+
+declare t6;
+  wait t4_c;
+  begin;
+  select * from persons;
+  commit;
+enddecl t6;
+
+run t4, t5, t6;
