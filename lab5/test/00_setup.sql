0487454ae99ddc9df987ccc31b68405e8fd18a16
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2022-05-07 21:02:30 +0800

    Add number

--- lab5/test/00_setup.sql
+++ lab5/test/00_setup.sql
@@ -4,6 +4,4 @@ use dbtrain_test_lab5;
 
 create table persons(id int, name varchar(20), temperature float);
 
-insert into persons values(1, 'a', 36.1);
-insert into persons values(2, 'b', 36.2);
-insert into persons values(3, 'c', 36.3);
+insert into persons values(1, 'a', 36.1), (2, 'b', 36.2), (3, 'c', 36.3);
