e3ce2e76c77e65bb008c4c9059558f5eac3f23ea
Xiang-cd <xcd19@mails.tsinghua.edu.cn>
2023-03-15 18:31:44 +0800

    load and store of meta table done

--- src/table/table_meta.cpp
+++ src/table/table_meta.cpp
@@ -1,22 +1,61 @@
 #include "table_meta.h"
 
 #include "record/fields.h"
-
+#include "utils/debug-print.hpp"
+#include <iostream>
+#include <vector>
 namespace dbtrain {
 
 int TableMeta::Load(const uint8_t *src) {
-  // TODO: 元信息反序列化
   // TIPS: 函数目标是通过src反序列化成Meta结构体，通过memcpy函数可以完成这一工作
   // TIPS: 需要注意将所有信息全部保存，变长信息可以通过“长度+数据”的方式存储
   // LAB 1 BEGIN
+  vector<Column>().swap(cols_);
+  int index = 0;
+  unsigned col_num = *(unsigned *) src;
+  Print("loaded col_num:", col_num);
+  index += sizeof (unsigned );
+  for (int i = 0; i < col_num; ++i) {
+      Column col = Column();
+      memcpy(&col, src+index, 16);
+      index += 16;
+      unsigned str_len = *(unsigned *) (src + index);
+      index += sizeof (unsigned );
+//      Print("loaded str len:", str_len);
+      char name[str_len];
+      memcpy(&name, src + index, str_len);
+      col.name_ = string(name);
+//      Print("name:", col.name_);
+      index += str_len;
+      cols_.push_back(col);
+  }
+
+  return 0;
   // LAB 1 END
 }
 
 int TableMeta::Store(uint8_t *dst) {
-  // TODO: 元信息序列化
   // TIPS: 函数目标是将Meta信息序列化到dst中，通过memcpy函数可以完成这一工作
   // TIPS: 需要注意将所有信息全部读取，变长信息需要结合Load函数解析
   // LAB 1 BEGIN
+    int index = 0;
+    unsigned col_num = cols_.size();
+    memcpy(dst, &col_num, sizeof (unsigned ));
+//    Print("stroed col_num:", col_num);
+    index += sizeof (unsigned );
+
+    for (auto col: cols_){
+        memcpy(dst+index, &col, 16);
+        index += 16;
+        unsigned str_len = col.name_.size() + 1;
+//        Print("str len:", str_len);
+        memcpy(dst+index, &str_len, sizeof (unsigned ));
+        index += sizeof (unsigned );
+//        Print("name:", col.name_);
+        strcpy((char*)dst+index, col.name_.c_str());
+        index += str_len;
+    }
+    return  0;
   // LAB 1 END
 }
 
---
+++ src/utils/debug-print.hpp
@@ -0,0 +1,29 @@
+//
+// Created by 项小羽 on 2023/3/15.
+//
+
+#ifndef DBTRAIN_DEBUG_PRINT_HPP
+#define DBTRAIN_DEBUG_PRINT_HPP
+
+
+#define Debug
+#include <iostream>
+
+template<typename T>
+void Print(const T& t) {
+#ifdef Debug
+  std::cout << t << std::endl;
+#endif
+}
+
+template<typename T, typename... args>
+void Print(const T& t, const args&... param) {
+#ifdef Debug
+  std::cout << t ;
+  Print(param...);
+#endif
+}
+
+
+
+#endif  // DBTRAIN_DEBUG_PRINT_HPP
