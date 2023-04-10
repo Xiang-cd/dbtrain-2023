1417a19114b0cc5654d05b20acf100192e57776f
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2023-04-10 14:50:01 +0800

    Add lab 3 comments for xid store and load

--- src/log/checkpoint_log.cpp
+++ src/log/checkpoint_log.cpp
@@ -11,6 +11,9 @@ CheckpointLog::CheckpointLog(LSN lsn) : Log(lsn) {}
 void CheckpointLog::Load(const Byte *src) {
   Log::Load(src);
   LogManager &log_manager = LogManager::GetInstance();
+  // TODO: 恢复当前事务编号
+  // LAB 3 BEGIN
+  // LAB 3 END
   // TODO: 加载MasterRecord对应的Checkpoint Log
   // TIPS: 利用读取的信息更新LogManager
   // LAB 2 BEGIN
@@ -20,6 +23,9 @@ void CheckpointLog::Load(const Byte *src) {
 size_t CheckpointLog::Store(Byte *dst) {
   LogManager &log_manager = LogManager::GetInstance();
   size_t fsize = Log::Store(dst);
+  // TODO: 存储当前事务编号
+  // LAB 3 BEGIN
+  // LAB 3 END
   // TODO: 存储LogManager相关信息，返回Store的数据长度
   // TIPS: 不添加缓存机制情况下，仅需要保存ATT和DPT
   // TIPS: 考虑缓存机制情况下，需要额外存储Flushed LSN
